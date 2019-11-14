d6a59f2d5cc6be601ae1dfbe5c02930dda57c200
Luuk S. van Houdt <info@lsvh.org>
2019-11-14 17:55:39 +0200

    Auto delete dist directory to fix the build on travis-ci

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import commonjs from 'rollup-plugin-commonjs'
 import external from 'rollup-plugin-peer-deps-external'
 import resolve from 'rollup-plugin-node-resolve'
 import url from 'rollup-plugin-url'
+import del from 'rollup-plugin-delete'
 
 import pkg from './package.json'
 
@@ -23,6 +24,7 @@ export default {
     }
   ],
   plugins: [
+    del({targets: 'dist/*'}),
     external(),
     url(),
     resolve(),
